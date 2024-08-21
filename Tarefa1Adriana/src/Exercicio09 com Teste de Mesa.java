404bcfd56aa99d9792e84fc033107288834e8a9d
Rafael Slivka <129512938+rafaslivka@users.noreply.github.com>
2024-08-21 11:00:45 -0300

    Update Exercicio09 com Teste de Mesa.java

--- Tarefa1Adriana/src/Exercicio09 com Teste de Mesa.java
+++ Tarefa1Adriana/src/Exercicio09 com Teste de Mesa.java
@@ -1,4 +1,5 @@
-9 - As maçãs custam R$ 1,30 cada se forem compradas menos de uma dúzia, e R$ 1,00 se forem
+9 - As maçãs custam R$ 1,30 cada se forem compradas menos de uma dúzia, e R$ 1,00 se forem compradas pelo menos doze. 
+    Escreva um programa que leia o número de maçãs compradas, calcule e escreva o custo total da compra. 
 
 import java.util.Scanner;
 
